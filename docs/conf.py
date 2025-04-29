acae3fad362a35e27058c0ebf9f563146ae1ee3e
Slava Poliakov <78slavapolyakov@gmail.com>
2025-04-29 21:34:53 +0300

    Fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -5,23 +5,26 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys
+from pathlib import Path
 
-project = 'poliakov_hw_12'
-copyright = '2025, poliakov'
-author = 'poliakov'
+sys.path.append(str(Path("..").resolve()))
+
+project = "poliakov_hw_12"
+copyright = "2025, poliakov"
+author = "poliakov"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
---
+++ docs/content.rst
@@ -0,0 +1,60 @@
+Project Modules
+===============
+
+Main App
+--------
+
+.. automodule:: app.main
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Authentication
+--------------
+
+.. automodule:: app.routes.auth
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+CRUD Operations
+---------------
+
+.. automodule:: app.services.crud
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Cloudinary Service
+------------------
+
+.. automodule:: app.services.cloudinary
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Database
+--------
+
+.. automodule:: app.repository.database
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Data Models
+-----------
+
+.. automodule:: app.repository.models
+   :members:
+   :undoc-members:
+   :show-inheritance:
+   :no-index:
+
+Schemas
+-------
+
+.. automodule:: app.repository.schemas
+   :members:
+   :undoc-members:
+   :show-inheritance:
+   :no-index:
